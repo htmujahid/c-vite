46646ee6f125ff5f0530fe57c0590bb9f375ea4b
Talha Mujahid <htmujahid@gmail.com>
2024-03-30 00:33:00 +0500

    feat: some more elements

---
+++ core/HtmlDom/CHTMLBodyElement.ts
@@ -0,0 +1,16 @@
+import CHTMLElement from './CHTMLElement';
+
+class CHTMLBodyElement extends CHTMLElement<HTMLBodyElement> {
+  public el: HTMLBodyElement;
+
+  constructor() {
+    super();
+    const html = document.createElement('body');
+    this.el = html;
+  }
+
+}
+
+export function body() {
+  return new CHTMLBodyElement();
+}
