6fa06e879290689b9a2471995b6370ac7fb048d2
Talha Mujahid <htmujahid@gmail.com>
2024-05-02 19:54:39 +0500

    feat: hash id support

--- src/App.ts
+++ src/App.ts
@@ -3,7 +3,20 @@ import Counter from './counter';
 
 class App extends SonnetComponent {
   get() {
-    return Counter().get();
+    return /*html*/ `
+      <div>
+        <a href="https://vitejs.dev" target="blank">
+          <img src="https://vitejs.dev/logo.svg" class="logo" alt="Vite Logo" />
+        </a>
+        <h1>Vite</h1>
+        <div class="card">
+          ${Counter().get()}
+          ${Counter().get()}
+          ${Counter().get()}
+        </div>
+        <p class="read-the-docs">Edit src/main.ts and save to test HMR.</p>
+      </div>
+    `;
   }
 }
 
--- src/counter.ts
+++ src/counter.ts
@@ -1,11 +1,10 @@
 import { $component, SonnetComponent } from '@sonnetjs/core';
-import { a, button, div, h1, img, p } from '@sonnetjs/html';
 
 class Counter extends SonnetComponent {
   counter = 0;
 
   script(): void {
-    const button = document.getElementById('counter') as HTMLButtonElement;
+    const button = this.parent.querySelector('#counter') as HTMLButtonElement;
 
     button.addEventListener('click', () => {
       this.counter += 1;
@@ -14,37 +13,11 @@ class Counter extends SonnetComponent {
   }
 
   get() {
-    return div()
-      .children(
-        a()
-          .href('https://vitejs.dev')
-          .target('blank')
-          .children(
-            img()
-              .src('https://vitejs.dev/logo.svg')
-              .className('logo')
-              .alt('Vite Logo')
-              .get(),
-          )
-          .get(),
-        h1().innerText('Vite').get(),
-        div()
-          .className('card')
-          .children(
-            button()
-              .id('counter')
-              .type('button')
-              .innerText(`count is ${this.counter}`)
-              .className('btn')
-              .get(),
-          )
-          .get(),
-        p()
-          .innerText('Edit src/main.ts and save to test HMR.')
-          .className('read-the-docs')
-          .get(),
-      )
-      .get();
+    return /*html*/ `
+      <div id="${this.hashId}">
+        <button id="counter" type="button" class="btn">count is ${this.counter}</button>
+      </div>
+    `;
   }
 }
 
